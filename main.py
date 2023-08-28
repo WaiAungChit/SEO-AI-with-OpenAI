19d394497289a4aac686d1d7320ce2eed9725741
WaiAungChit <weiaungchit14@gmail.com>
2023-08-28 11:50:58 +0700

    Heroku Profile Added

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import routers.route as generate_routes
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(generate_routes.router)
